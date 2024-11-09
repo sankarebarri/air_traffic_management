541d15d7d79b8e8d4c5c014a571c4c787a94ab36
sankarebarri <sankarebarri_dev@yahoo.com>
2024-11-09 10:11:08 +0000

    flight tables popualted with the backend data

--- air_traffic_management/flights/views.py
+++ air_traffic_management/flights/views.py
@@ -3,13 +3,25 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .models import Flights
 
+from django.db.models import Q
+
 class FlightListView(generic.ListView):
     model = Flights
     template_name = 'flights/flight_list.html'
     context_object_name = 'flights'
 
 def scheduled_flights_view(request):
-    return render(request, 'flights/scheduled_flights.html')
+
+    context = {
+        'flights': Flights.objects.filter(Q(status='Scheduled') | Q(status='Delayed') | Q(status='Cancelled'))
+    }
+
+    return render(request, 'flights/scheduled_flights.html', context)
 
 def current_flights_view(request):
-    return render(request, 'flights/current_flights.html')
+
+    context = {
+        'flights': Flights.objects.filter(Q(status='Departed') | Q(status='Arrived'))
+    }
+
+    return render(request, 'flights/current_flights.html', context)
