bee2067e09dc9efdc1f3bf285dc651bcb06bcd58
sankarebarri <sankarebarri_dev@yahoo.com>
2024-11-12 15:33:05 +0000

    Deploying to render.com configuarations

--- air_traffic_management/air_traffic_management/settings.py
+++ air_traffic_management/air_traffic_management/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG','False') == 'True'
+DEBUG = os.getenv('DEBUG','False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
